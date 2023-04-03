e38f518234412375abf28a48d33a1e1f2d56a2a9
veysiulger <veysiulger@gmail.com>
2023-04-03 23:18:44 +0300

    lec4, lec5

---
+++ lec4_flow_controls/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec4_flow_controls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region switch + when/goto
+            /*
+            Switch ile sadece mevcut degerin, esitlik durumuna gore, 
+            ilgili case scope'indaki kodlar yurutulur.
+            */
+            char charVariables = 'w';
+            switch (charVariables)
+            {
+                case 'x':
+                    Console.WriteLine("karakter x ");
+                    break;
+                case 'y':
+                    Console.WriteLine("karakter y ");
+                    break;
+                case 'z' when (1 > 0):
+                    //when keyword eklenerek, switch'e kosul eklenebilir.
+                    Console.WriteLine("karakter z ");
+                    break;
+                case 'w':
+                    //goto ile dallanma yapilabilir.
+                    goto case 'x';
+                default:
+                    Console.WriteLine("hic eslesme yoktur");
+                    break;
+            }
+            #endregion
+
+            #region if-else
+            string logicalValue = " false";
+
+            if (logicalValue == "true")
+            {
+                Console.WriteLine(" positive=" + logicalValue);
+            }
+            else if (logicalValue == "false")
+            {
+                Console.WriteLine(" negative=" + logicalValue);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            #endregion
+
+            #region Pattern Matching
+
+            #region Type Pattern
+
+            object x = "500";
+            if(x is int varX)
+            {   //x int ise, varX=x olur.
+                Console.WriteLine($"x degiskeni int tipindedir.");
+            }else if (x is string varY)
+            {
+                //x str ise, varY=x olur.   
+                Console.WriteLine($"x degiskeni bir string'tir.");
+            }
+           
+
+            #endregion
+
+            #region Constant Pattern
+            /* Elimizdeki veriyi sabit ile kiyaslayabiliriz. 
+             */
+
+            int varZ = 431;
+            Console.WriteLine(varZ is 431);//true yazar.
+
+            #endregion
+
+            #region Var Pattern
+            /* Elimizdeki veriyi var degiskeni ile elde etmemizi 
+             * saglamaktadir.
+             */
+            object varAa = "Lorem ipsum";
+            if (varAa is var varBb)
+            {
+                Console.WriteLine(varBb);
+
+            }
+            #endregion
+
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lec5_error_control_mechanism/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec5_error_control_mechanism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Runtime Error
+            /* Calisma zamani hatalari. Kodun semantiginde, syntax'ta hata 
+             * olmaz. Kod calisma esnasinda OS mudahalesi ile, hata verir. 
+             * Olasi hatalari goz onune alarak, son kullaniciya hitaben, 
+             * daha anlasilir bir sekilde olasi hatalar duzenlenmelidir.
+             * 
+             * try-cathc: Bu yapilanma ile uygulama surecinde, olasi hatalari
+             * yonetebiliriz. Hatalari manipule etmeye yarar. Amaci,
+             * kullaniciya hatalari hissettirmeden bildirmek.
+             * 
+             * try{ Olasi hata verebilecek kodlar buraya yazilir. }
+             * catch{ try icinde hata veren kod orada kesilir, catch'den devam eder.}
+             * 
+             * NOT: Bu yapilanma, icindeki kodlari surekli denetler.
+             * Yani hep tetiklenmektedir. Bu sebeple maliyet olusturur. Bu sebeple 
+             * luzumsuz kodlari try icerisine yazmamamiz gerekiyor. Sadece 
+             * hata verme ihtimali olan kodlar buraya gomulmelidir.
+             */
+            int sayi1=1, sayi2=1;
+            try
+            {   
+                Console.WriteLine("birinci sayi:");
+                 sayi1=int.Parse(Console.ReadLine());//gelen sayi int'e cevrildi.
+
+                Console.WriteLine("ikinci sayi:");
+                 sayi2 = int.Parse(Console.ReadLine());
+
+               
+                /* Kullanici burada str girebilir. 
+                 * O halde uygulama patlayacaktir. 
+                 */
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Sadece tam sayi giriniz!");
+                Console.WriteLine("Hata mesaji: "+ex.Message);
+
+            }
+            //Birden fazla catch eklenebilir.
+            //Exeption: Tum hatalarin ecdadi. En sona yazilir.
+            /* catch(DivideByZeroExeption ex){
+             * 
+             * } 
+             */
+            Console.WriteLine("Carpim: " + sayi1 * sayi2);
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
